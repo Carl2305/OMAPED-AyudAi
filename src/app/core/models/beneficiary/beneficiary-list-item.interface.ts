42c50e1c0eda3bea6b8357195123d6a713ff0578
CARLOS MOGOLLON ESPINOZA <mogollon.23carlos05@gmail.com>
2025-10-31 20:16:51 -0500

    Listado de beneficiarios registrados

---
+++ src/app/core/models/beneficiary/beneficiary-list-item.interface.ts
@@ -0,0 +1,18 @@
+export interface BeneficiaryListItem {
+  tipoDocumento: string;
+  numeroDocumento: string;
+  nombreCompleto: string;
+  tipoDiscapacidad: string;
+  serviciosBasicos: string | null;
+  edad: number;
+}
+
+export interface PagedResponse<T> {
+  items: T[];
+  pageNumber: number;
+  pageSize: number;
+  totalCount: number;
+  totalPages: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+}
